b0d302392ea499a502222c42c4b36b8dc3d2dba0
Nurmurad32 <nur.murad32@gmail.com>
2025-06-21 16:39:42 +0600

    remove image and fix eslint for img

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,19 +1,23 @@
 import { fetchProductById } from '@/lib/api'
-import { notFound } from 'next/navigation'
 import ProductDetailsContent from './ProductDetailsContent'
+import { notFound } from 'next/navigation'
 import Loading from './loading'
 
 type Props = {
-  params: { id: string }
+    params: {
+        id: string
+    }
 }
 
-export default async function ProductDetailsPage({ params }: Props) {
-    const { id } = params
-    const product = await fetchProductById(id)
+export default async function ProductPage({ params }: Props) {
+    const product = await fetchProductById(params.id)
+
     if (!product) return <Loading />
     if (!product?.id) return notFound()
 
     return (
-        <ProductDetailsContent product={product} />
+        <main className="p-6 max-w-5xl mx-auto">
+            <ProductDetailsContent product={product} />
+        </main>
     )
 }
